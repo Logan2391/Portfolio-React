25a6524e5f3d98819436ff486512dc2f2110ebf8
Combatxd <logan.schoerner@gmail.com>
2022-10-30 18:20:42 -0600

    inital file setups

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import PortContain from './components/PortfolioContainer'
+
+function App() {
+  return <PortContain />;
+}
+
+export default App;
---
+++ src/components/PortfolioContainer.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import Nav from './Nav';
+import Resume from './pages/Resume';
+import About from './pages/About';
+import Projects from './pages/Project';
+import Contact from './pages/Contact';
+
+export default function PortfolioContainer() {
+    const [currentPage, setCurrentPage] = useState('About');
+
+    const renderPage = () => {
+        if (currentPage === 'About') {
+            return <About />;
+        }
+        if (currentPage === 'Projects') {
+            return <Projects />;
+        }
+        if (currentPage === 'Resume') {
+            return <Resume />;
+        }
+        return <Contact />;
+    };
+
+    const handlePageChange = (page) => setCurrentPage(page);
+
+    return (
+        <div>
+            <Nav currentPage={currentPage} handlePageChange={handlePageChange} />
+            {renderPage()}
+        </div>
+    );
+}
