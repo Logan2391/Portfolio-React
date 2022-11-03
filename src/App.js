820593992d2cbf6d4e85990cfa5fa47d096bb916
Combatxd <logan.schoerner@gmail.com>
2022-11-03 13:59:17 -0600

    Updates

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import PortContain from './components/PortfolioContainer'
-import styleSheet from './style.css';
-
-function App() {
-  return <PortContain />;
-}
-
-export default App;
